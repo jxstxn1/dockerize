b5cb468ef68cc121ff48eaf1f08a20ac3ff2b685
jxstxn__ <justin.baumann@t-online.de>
2022-12-04 15:23:35 +0100

    Create files in command/dockerize

--- tool/install.dart
+++ tool/install.dart
@@ -1,4 +1,5 @@
-import 'package:sidekick_core/sidekick_core.dart' hide cliName, repository, mainProject;
+import 'package:sidekick_core/sidekick_core.dart'
+    hide cliName, repository, mainProject;
 import 'package:sidekick_plugin_installer/sidekick_plugin_installer.dart';
 
 import 'template.dart';
@@ -12,24 +13,35 @@ Future<void> main() async {
     // For local development
     pubAddLocalDependency(package, PluginContext.localPlugin!.root.path);
   }
+  await createServerFolder(package);
   pubGet(package);
-
-  final commandFile = package.root.file('lib/src/commands/dockerize_command.dart');
-  commandFile.writeAsStringSync(dockerizeFile);
-
-  final folder = package.root.directory('../server');
-  if (!folder.existsSync()) {
-    folder.createSync();
-    folder.directory('bin').createSync();
+  final commandFolder = package.root.directory('lib/src/commands/dockerize');
+  if (!commandFolder.existsSync()) {
+    commandFolder.createSync();
   }
-  folder.directory('bin').file('server.dart').writeAsStringSync(serverFile);
-  folder.file('Dockerfile').writeAsStringSync(dockerFile);
-  folder.file('pubspec.yaml').writeAsStringSync(pubspecFile);
-  pubGet(DartPackage(folder, 'server'));
+
+  final commandFile = commandFolder.file('dockerize_sidekick_plugin.dart');
+  commandFile.writeAsStringSync(dockerizeFile);
 
   registerPlugin(
     sidekickCli: package,
-    import: "import 'package:${package.name}/src/commands/dockerize_command.dart';",
+    import:
+        "import 'package:${package.name}/src/commands/dockerize/dockerize_sidekick_plugin.dart';",
     command: 'Dockerize()',
   );
+}
+
+Future<void> createServerFolder(SidekickPackage package) async {
+  final serverFolder = package.root.directory('../server');
+  if (!serverFolder.existsSync()) {
+    serverFolder.createSync();
+    serverFolder.directory('bin').createSync();
+  }
+  serverFolder
+      .directory('bin')
+      .file('server.dart')
+      .writeAsStringSync(serverFile);
+  serverFolder.file('Dockerfile').writeAsStringSync(dockerFile);
+  serverFolder.file('pubspec.yaml').writeAsStringSync(pubspecFile);
+  pubGet(DartPackage(serverFolder, 'server'));
 }
