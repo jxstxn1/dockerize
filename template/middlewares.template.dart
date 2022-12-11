baa588530d16de742f62b7b4aa41e5018fecb806
jxstxn__ <justin.baumann@t-online.de>
2022-12-11 13:25:45 +0100

    Upgrade to new Sidekick Plugin Installer Version

---
+++ template/middlewares.template.dart
@@ -0,0 +1,40 @@
+import 'package:shelf/shelf.dart';
+import 'package:shelf_helmet/shelf_helmet.dart';
+
+/// Returns a opinionated set of middlewares for a shelf server.
+/// This is used by default from the server.dart file.
+Middleware middlewares() {
+  Pipeline pipeline = Pipeline();
+
+  // Logging middleware
+  pipeline = pipeline.addMiddleware(logRequests());
+
+  // Helmet middleware
+  // You can customize or remove the default helmet middleware
+  // For more information checkout https://pub.dev/packages/shelf_helmet
+  pipeline = pipeline.addMiddleware(
+    helmet(
+      options: HelmetOptions(
+        cspOptions: ContentSecurityPolicyOptions.useDefaults(
+          directives: {
+            'script-src': [
+              "'unsafe-eval'",
+              "'unsafe-inline'",
+              "'self'",
+              "blob:",
+              "https://unpkg.com/",
+            ],
+            'connect-src': [
+              "'self'",
+              "https://unpkg.com/",
+              "https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Me5WZLCzYlKw.ttf ",
+            ],
+          },
+        ),
+        coepOptions: CrossOriginEmbedderPolicyOptions.credentialLess,
+      ),
+    ),
+  );
+
+  return (innerHandler) => pipeline.addHandler(innerHandler);
+}
---
+++ tool/replace_template_dependencies.dart
@@ -0,0 +1,19 @@
+// Function to replace the template dependencies with the installed dependencies
+List<String> replaceTemplateDependencies(
+  List<String> content,
+  String packageName,
+) {
+  return content
+      .map(
+        (element) => element.replaceAll(
+          '<<packageName>>',
+          '${packageName}_sidekick',
+        ),
+      )
+      .toList()
+    ..removeWhere(
+      (element) =>
+          element.contains('//template import') ||
+          element.contains('installed import'),
+    );
+}
