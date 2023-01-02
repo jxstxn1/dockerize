19f74265f1c36eeaae7e4a58ff55ff657f98bf37
jxstxn__ <justin.baumann@t-online.de>
2023-01-02 15:16:11 +0100

    Add to export

--- lib/dockerize_sidekick_plugin.dart
+++ lib/dockerize_sidekick_plugin.dart
@@ -6,6 +6,7 @@ export 'src/docker/create_image.dart';
 export 'src/docker/run_image.dart';
 export 'src/docker/stop_image.dart';
 export 'src/util/check_docker_install.dart';
+export 'src/util/enforce_csp.dart';
 export 'src/util/hash_scripts.dart' show hashScripts;
 export 'src/util/move_to_server_directory.dart';
 export 'src/util/print_usage.dart';
