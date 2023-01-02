66656ee5fd42d52148519a8a86eb8065cd4d3460
jxstxn__ <justin.baumann@t-online.de>
2023-01-02 15:15:47 +0100

    Add enforceCSP Method

---
+++ lib/src/util/enforce_csp.dart
@@ -0,0 +1,17 @@
+import 'package:sidekick_core/sidekick_core.dart';
+
+void enforceCSP({required bool shouldEnforce}) {
+  final middlewareFile =
+      repository.root.directory('server/bin').file('middlewares.dart');
+  final middlewareFileContent = middlewareFile.readAsStringSync();
+  final RegExp regex = RegExp(
+    'const bool shouldEnforceCsp = (.*?);',
+    multiLine: true,
+    dotAll: true,
+  );
+  final String newContent = middlewareFileContent.replaceAll(
+    regex,
+    'const bool shouldEnforceCsp = $shouldEnforce;',
+  );
+  middlewareFile.writeAsStringSync(newContent);
+}
