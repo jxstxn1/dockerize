f5acfae240aa52349e97da59fb3c515a33a895ec
jxstxn__ <justin.baumann@t-online.de>
2023-01-02 16:40:26 +0100

    Add early return if we dont enforceCSP

--- lib/src/util/enforce_csp.dart
+++ lib/src/util/enforce_csp.dart
@@ -1,6 +1,7 @@
 import 'package:sidekick_core/sidekick_core.dart';
 
 void enforceCSP({required bool shouldEnforce}) {
+  if (!shouldEnforce) return;
   final middlewareFile =
       repository.root.directory('server/bin').file('middlewares.dart');
   final middlewareFileContent = middlewareFile.readAsStringSync();
