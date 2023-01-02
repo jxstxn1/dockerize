ba36b19d9075edb486b1c874ed635dfc34a5fcf8
jxstxn__ <justin.baumann@t-online.de>
2023-01-02 15:16:35 +0100

    Add envName to createImage

--- lib/src/docker/create_image.dart
+++ lib/src/docker/create_image.dart
@@ -2,14 +2,14 @@ import 'package:dockerize_sidekick_plugin/src/util/command_runner.dart';
 import 'package:sidekick_core/sidekick_core.dart';
 
 /// Creates a docker image
-void createDockerImage() {
+void createDockerImage(String environmentName) {
   print(
-    green('Creating image ${mainProject!.name}:dev'),
+    green('Creating image ${mainProject!.name}:$environmentName'),
   );
   commandRunner(
     'docker',
-    ['build', '-t', '${mainProject!.name}:dev', '.'],
+    ['build', '-t', '${mainProject!.name}:$environmentName', '.'],
     workingDirectory: repository.root.directory('server'),
-    successMessage: 'Created image ${mainProject!.name}:dev',
+    successMessage: 'Created image ${mainProject!.name}:$environmentName',
   );
 }
