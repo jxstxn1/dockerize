4ea29b03568ad66bd9517bcca8716cab6a093d81
jxstxn__ <justin.baumann@t-online.de>
2023-01-03 11:10:47 +0100

    Kill on sigint

--- lib/src/docker/stop_image.dart
+++ lib/src/docker/stop_image.dart
@@ -2,12 +2,17 @@ import 'package:dockerize_sidekick_plugin/src/util/command_runner.dart';
 import 'package:sidekick_core/sidekick_core.dart';
 
 /// Stoping the currently running docker image
-void stopImage({bool silent = false}) {
+void stopImage({
+  bool silent = false,
+  String? mainProjectName,
+  Directory? workingDirectory,
+}) {
+  final projectName = mainProjectName ?? mainProject!.name;
   commandRunner(
     'docker',
-    ['kill', mainProject!.name],
-    workingDirectory: repository.root.directory('server'),
+    ['kill', projectName],
+    workingDirectory: workingDirectory ?? repository.root.directory('server'),
     silent: silent,
-    successMessage: 'Stopped app: ${mainProject!.name}',
+    successMessage: 'Stopped app: $projectName',
   );
 }
