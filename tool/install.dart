417b2802fb38d1dbf23966ac4552c6c5caca0944
jxstxn__ <justin.baumann@t-online.de>
2023-01-03 15:13:05 +0100

    Add createIfNotExists method to simplify checking for existing/creating folders

--- tool/install.dart
+++ tool/install.dart
@@ -1,7 +1,7 @@
-import 'package:sidekick_core/sidekick_core.dart'
-    hide cliName, repository, mainProject;
+import 'package:sidekick_core/sidekick_core.dart' hide cliName, repository, mainProject;
 import 'package:sidekick_plugin_installer/sidekick_plugin_installer.dart';
 
+import 'create_if_not_exists.dart';
 import 'create_server_folder.dart';
 import 'replace_template_dependencies.dart';
 
@@ -11,32 +11,23 @@ Future<void> main() async {
   await createServerFolder(package);
 
   pubGet(package);
-  final commandFolder = package.root.directory('lib/src/commands/dockerize');
-  if (!commandFolder.existsSync()) {
-    commandFolder.createSync();
-  }
+  final commandFolder = package.root.directory('lib/src/commands/dockerize').createIfNotExists();
 
   final dockerCommandFile = commandFolder.file('docker_command.dart');
   dockerCommandFile.writeAsStringSync(
     replaceTemplateDependencies(
-      PluginContext.installerPlugin.root
-          .file('template/docker_command.template.dart')
-          .readAsLinesSync(),
+      PluginContext.installerPlugin.root.file('template/docker_command.template.dart').readAsLinesSync(),
       package.cliName,
     ).join('\n'),
   );
 
   final environmentFile = commandFolder.file('environment.dart');
-  PluginContext.installerPlugin.root
-      .file('template/environment.template.dart')
-      .copySync(environmentFile.path);
+  PluginContext.installerPlugin.root.file('template/environment.template.dart').copySync(environmentFile.path);
 
   final buildCommandFile = commandFolder.file('build_command.dart');
   buildCommandFile.writeAsStringSync(
     replaceTemplateDependencies(
-      PluginContext.installerPlugin.root
-          .file('template/build_command.template.dart')
-          .readAsLinesSync(),
+      PluginContext.installerPlugin.root.file('template/build_command.template.dart').readAsLinesSync(),
       package.cliName,
     ).join('\n'),
   );
@@ -44,22 +35,17 @@ Future<void> main() async {
   final runCommandFile = commandFolder.file('run_command.dart');
   runCommandFile.writeAsStringSync(
     replaceTemplateDependencies(
-      PluginContext.installerPlugin.root
-          .file('template/run_command.template.dart')
-          .readAsLinesSync(),
+      PluginContext.installerPlugin.root.file('template/run_command.template.dart').readAsLinesSync(),
       package.cliName,
     ).join('\n'),
   );
 
   final stopCommandFile = commandFolder.file('stop_command.dart');
-  PluginContext.installerPlugin.root
-      .file('template/stop_command.template.dart')
-      .copySync(stopCommandFile.path);
+  PluginContext.installerPlugin.root.file('template/stop_command.template.dart').copySync(stopCommandFile.path);
 
   await registerPlugin(
     sidekickCli: package,
-    import:
-        "import 'package:${package.name}/src/commands/dockerize/docker_command.dart';",
+    import: "import 'package:${package.name}/src/commands/dockerize/docker_command.dart';",
     command: 'DockerCommand()',
   );
   print(
