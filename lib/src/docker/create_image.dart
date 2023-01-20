77b623b253fbc50b99e3e7847ce1e1cfa095f52d
jxstxn__ <justin.baumann@t-online.de>
2023-01-20 16:55:19 +0100

    Simplify create image

--- lib/src/docker/create_image.dart
+++ lib/src/docker/create_image.dart
@@ -1,4 +1,3 @@
-import 'dart:io' as io;
 import 'package:dockerize_sidekick_plugin/src/util/command_runner.dart';
 import 'package:mason_logger/mason_logger.dart';
 import 'package:sidekick_core/sidekick_core.dart' hide Progress;
@@ -9,62 +8,25 @@ Future<void> createDockerImage(
   Logger? logger,
   bool buildScripts = true,
   bool buildFlutter = true,
+  Directory? workingDirectoryPath,
 }) async {
   final Logger buildLogger = logger ?? Logger();
   buildLogger.info(
     '[dockerize] Creating image ${mainProject!.name}:$environmentName',
   );
+  final workingDir =
+      (workingDirectoryPath ?? repository.root).directory('server');
   await commandRunner(
     'docker',
     [
       'buildx',
       'build',
-      '--build-arg',
-      'enviroment=$environmentName',
-      '--build-arg',
-      'build_flutter=$buildFlutter',
-      '--build-arg',
-      'build_scripts=$buildScripts',
       '-t',
       '${mainProject!.name}:$environmentName',
       '.',
     ],
     logger: buildLogger,
-    workingDirectory: repository.root.directory('server'),
+    workingDirectory: workingDir,
     successMessage: 'Created image ${mainProject!.name}:$environmentName 🎉',
   );
-}
-
-// Helper method which is under the hood calling the build command
-Future<void> executeBuild({
-  Progress? progressLogger,
-  required bool buildContainer,
-  required String envName,
-  String? path,
-}) async {
-  final Logger buildLogger = Logger();
-  final entryPointPath = path ?? Repository.requiredEntryPoint.path;
-  progressLogger?.update('[dockerize] Building app');
-  final process = await io.Process.run(
-    entryPointPath,
-    [
-      'docker',
-      'build',
-      '--env=$envName',
-      if (buildContainer) '--docker-only',
-    ],
-  );
-  if (process.exitCode == 0) {
-    if (progressLogger != null) {
-      progressLogger.update('[dockerize] Build successful');
-    } else {
-      buildLogger.success('[dockerize] Build successful');
-    }
-  } else {
-    if (progressLogger != null) {
-      progressLogger.fail('[dockerize] Build failed');
-    } else {
-      buildLogger.err('[dockerize] Build failed');
-    }
-  }
 }
