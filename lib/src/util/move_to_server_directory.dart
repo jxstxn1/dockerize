7d116282c5edc00e1ce8873e297b57c621c5a2ad
jxstxn__ <justin.baumann@t-online.de>
2023-01-25 11:23:32 +0100

    Migrate MoveToServerDirectory

--- lib/src/util/move_to_server_directory.dart
+++ lib/src/util/move_to_server_directory.dart
@@ -2,10 +2,12 @@ import 'package:sidekick_core/sidekick_core.dart';
 
 // This moves the build web project to the server/www directory
 void moveToServerDirectory() {
-  repository.root.directory('server/www').createSync(recursive: true);
+  SidekickContext.projectRoot
+      .directory('server/www')
+      .createSync(recursive: true);
   copyTree(
     mainProject!.root.directory('build/web').path,
-    repository.root.directory('server').directory('www').path,
+    SidekickContext.projectRoot.directory('server').directory('www').path,
     overwrite: true,
   );
 }
