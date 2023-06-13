8f10616bcf55b5b3c9275f8a3dfb0e902fcb62af
Justin Baumann <justin.baumann@t-online.de>
2023-06-13 11:59:31 +0200

    Bump version

--- tool/create_server_folder.dart
+++ tool/create_server_folder.dart
@@ -1,5 +1,5 @@
 import 'package:sidekick_core/sidekick_core.dart'
-    hide cliName, repository, mainProject;
+    hide cliName, mainProject, repository;
 import 'package:sidekick_plugin_installer/sidekick_plugin_installer.dart';
 import 'create_if_not_exists.dart';
 import 'replace_template_dependencies.dart';
